3ead5cb94d4c079a77eaa197aabc35656964af26
tcwofs <tcwofs@users.noreply.github.com>
2020-08-21 13:46:20 +0300

    fix: :bug: replace BrowserRouter with HashRouter for gh-pages

--- src/components/router/Router.jsx
+++ src/components/router/Router.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { HashRouter as Router, Route, Switch } from 'react-router-dom';
 import { Cube, Home, Nebula, Particles, Rain } from '../../pages';
 import Drawer from '../drawer/Drawer';
 import { useStyles } from './Router.styles';
