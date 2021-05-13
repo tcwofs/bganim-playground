3fff269114b08c576f91261667796affd89cc6cd
tcwofs <liampicave@gmail.com>
2021-05-13 22:48:38 +0300

    feat: add fade transition to button

--- src/components/drawer/Drawer.jsx
+++ src/components/drawer/Drawer.jsx
@@ -1,15 +1,17 @@
-import { Button, Drawer, ListItemText } from '@material-ui/core';
+import { IconButton, Drawer, ListItemText, Fade } from '@material-ui/core';
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
 import MenuIcon from '@material-ui/icons/Menu';
 import clsx from 'clsx';
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import { useStyles } from './Drawer.style';
 
 const DrawerComponent = () => {
   const classes = useStyles();
-  const [state, setState] = React.useState(false);
+
+  const [checked, setChecked] = useState(true);
+  const [state, setState] = useState(false);
 
   const toggleDrawer = (open) => (event) => {
     if (event.type === 'keydown' && (event.key === 'Tab' || event.key === 'Shift')) {
@@ -17,6 +19,10 @@ const DrawerComponent = () => {
     }
 
     setState(open);
+  };
+
+  const handleChange = (value) => {
+    setChecked(value);
   };
 
   const list = () => (
@@ -48,17 +54,25 @@ const DrawerComponent = () => {
     </div>
   );
 
+  useEffect(() => {
+    setTimeout(() => {
+      setChecked(false);
+    }, 2000);
+  }, []);
+
   return (
-    <React.Fragment>
-      <div className={classes.menu}>
-        <Button onClick={toggleDrawer(true)}>
-          <MenuIcon />
-        </Button>
+    <>
+      <div className={classes.menu} onMouseOver={() => handleChange(true)} onMouseOut={() => handleChange(false)}>
+        <Fade in={checked}>
+          <IconButton onClick={toggleDrawer(true)} disableRipple disableFocusRipple disableTouchRipple style={{ backgroundColor: 'transparent' }}>
+            <MenuIcon />
+          </IconButton>
+        </Fade>
       </div>
       <Drawer open={state} onClose={toggleDrawer(false)}>
         {list()}
       </Drawer>
-    </React.Fragment>
+    </>
   );
 };
 
